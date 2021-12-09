c566a626e632bc416ea5391be6b440a99f15e741
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-12-09 21:47:48 +0300

    create test for the profile image and the comments models

--- instaapp/tests.py
+++ instaapp/tests.py
@@ -1,3 +1,102 @@
 from django.test import TestCase
 
 # Create your tests here.
+class ImageTestClass(TestCase):
+    """
+    Test Image class and its functions
+    """
+    def setUp(self):
+        #creating an new profile and saving it
+        self.profile = Profile(bio='This is my bio')
+        self.profile.save_profile()
+        #creating an new image
+        self.image = Image(post='test.jpg',caption='Fun times with bae', posted_on='Monday', profile=self.profile)
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image, Image))
+    def test_save_method(self):
+        """
+        Function to test that image is being saved
+        """
+        self.image.save_img()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
+    def test_delete_method(self):
+        """
+        Function to test that an image can be deleted
+        """
+        self.image.save_img()
+        self.image.del_img()
+    def test_update_method(self):
+        """
+        Function to test that an image's details can be updated
+        """
+        self.image.save_img()
+        new_image = Image.objects.filter(caption='Fun times with bae').update(caption='Fun times')
+        images = Image.objects.get(caption='Fun times')
+        self.assertTrue(images.caption, 'Fun times'
+        class ProfileTestClass(TestCase):
+    """
+    Test profile class and its functions
+    """
+    def setUp(self):
+        #creating an new profile
+        self.profile = Profile(bio='This is my bio', dp='name.jpg')
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile, Profile))
+    def test_save_method(self):
+        """
+        Function to test that profile is being saved
+        """
+        self.profile.save_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) > 0)
+    def test_delete_method(self):
+        """
+        Function to test that a profile can be deleted
+        """
+        self.profile.save_profile()
+        self.profile.del_profile()
+    def test_update_method(self):
+        """
+        Function to test that a profile's details can be updated
+        """
+        self.profile.save_profile()
+        new_profile = Profile.objects.filter(bio='This is my bio').update(bio='Now this is the real bio')
+        profiles = Profile.objects.get(bio='Now this is the real bio')
+        self.assertTrue(profiles.bio, 'Now this is the real bio')
+        class CommentsTestClass(TestCase):
+    """
+    Test Comments class and its functions
+    """
+    def setUp(self):
+         #creating an new profile and saving it
+        self.profile = Profile(bio='This is my bio')
+        self.profile.save_profile()
+        #creating an new image and saving it
+        self.image = Image(post='test.jpg',caption='Fun times with bae', posted_on='Monday', profile=self.profile)
+        self.image.save_img()
+        #creating a new comment and saving it
+        self.comm = Comments(comment='lol', posted_on='Monday', image=self.image)
+    def test_instance(self):
+        self.assertTrue(isinstance(self.comm, Comments))
+    def test_save_method(self):
+        """
+        Function to test that a comment is being saved
+        """
+        self.comm.save_comm()
+        feedback = Comments.objects.all()
+        self.assertTrue(len(feedback) > 0)
+    def test_delete_method(self):
+        """
+        Function to test that a comment can be deleted
+        """
+        self.comm.save_comm()
+        self.comm.del_comm()
+    def test_get_comment_by_image_id(self):
+        """
+        Function to test if you can get comments based on the image_id
+        """
+        self.comm.save_comm()
+        this_comm= self.comm.get_comments_by_image_id(self.comm.image_id)
+        comments = Comments.objects.get(id=self.comm.image_id)
+        self.assertTrue(this_comm, comments)
