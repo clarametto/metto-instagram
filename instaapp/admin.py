f9aa67f30cd8a8c5bbf1b7caf1eebdbe2865d6a2
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-12-09 15:09:25 +0300

    registered classes in the admit.py

--- instaapp/admin.py
+++ instaapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Profile, Image, Like,Follows,Comment
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Like)
+admin.site.register(Follows)
+admin.site.register(Comment)
+admin.site.register(Image)
+
+
